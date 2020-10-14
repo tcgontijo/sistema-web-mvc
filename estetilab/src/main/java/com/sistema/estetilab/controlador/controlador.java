e6e8eb36f1a993c99001fba287bc2f4fa2bb9d4b
Tulio Gontijo <tulio_gontijo@hotmail.com>
2020-10-14 20:12:36 -0300

    Inclusão do controlador

---
+++ estetilab/src/main/java/com/sistema/estetilab/controlador/controlador.java
@@ -0,0 +1,14 @@
+package com.sistema.estetilab.controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controlador {
+
+	//@RequestMapping("/")
+	//public String pag1() {
+	//	return "pagina1";
+	//}
+
+}
