17a1518f062254a4573ff37fc6c253e0ce1c88b9
Gontijo <Gontijo@TULIO_GONTIJO>
2020-10-14 19:58:26 -0300

    Criação do projeto

---
+++ estetilab/src/main/java/com/sistema/estetilab/EstetilabApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.estetilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstetilabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstetilabApplication.class, args);
+	}
+
+}
---
+++ estetilab/src/test/java/com/sistema/estetilab/EstetilabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.estetilab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstetilabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
